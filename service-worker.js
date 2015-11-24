799173bbfd8fcf6a2ff874012dde01971a860f28
Sean Amarasinghe <sean.amarasinghe@xero.com>
2015-11-24 11:13:31 +1100

    indented code

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,10 @@ self.addEventListener("fetch", function(event) {
   console.log('WORKER: fetch event in progress.');
 
   if (event.request.method !== 'GET') {
-    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+    console.log(
+      'WORKER: fetch event ignored.',
+      event.request.method, event.request.url
+    );
     return;
   }
 
@@ -31,28 +34,39 @@ self.addEventListener("fetch", function(event) {
         .then(fetchedFromNetwork, unableToResolve)
         .catch(unableToResolve);
 
-      console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+      console.log(
+        'WORKER: fetch event', cached ? '(cached)' :
+        '(network)', event.request.url
+      );
 
       return cached || networked;
 
       function fetchedFromNetwork(response) {
         var cacheCopy = response.clone();
 
-        console.log('WORKER: fetch response from network.', event.request.url);
+        console.log(
+          'WORKER: fetch response from network.',
+          event.request.url
+        );
         caches
           .open(version + 'pages')
           .then(function add(cache) {
             cache.put(event.request, cacheCopy);
           })
           .then(function() {
-            console.log('WORKER: fetch response stored in cache.', event.request.url);
+            console.log(
+              'WORKER: fetch response stored in cache.',
+              event.request.url
+            );
           });
 
           return response;
       }
 
       function unableToResolve () {
-        console.log('WORKER: fetch request failed in both cache and network.');
+        console.log(
+          'WORKER: fetch request failed in both cache and network.'
+        );
 
         return new Response('<h1>Service Unavailable</h1>', {
           status: 503,
